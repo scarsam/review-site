ed25ed31d05a4ee9ebed10b95a591bea39529255
Sam Ojling <s.ojling@gmail.com>
2018-04-21 14:56:56 +0200

    Setup my database tables and migrations

--- db/migrate/20180420112331_create_reviews.rb
+++ db/migrate/20180420112331_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
-
+      t.string :title
+      t.text :description
+      t.integer :rating
+      t.references :author
       t.timestamps
     end
   end
--- db/migrate/20180420112344_create_authors.rb
+++ db/migrate/20180420112344_create_authors.rb
@@ -1,7 +1,10 @@
 class CreateAuthors < ActiveRecord::Migration[5.1]
   def change
     create_table :authors do |t|
-
+      t.string :name
+      t.string :email
+      t.string :image
+      t.string :uid
       t.timestamps
     end
   end
