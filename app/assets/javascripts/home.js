b704ead3b75ea87b30e42fb40f07b1ff33779295
Sam Ojling <s.ojling@gmail.com>
2018-04-23 13:34:05 +0200

    Added class constructor, added click events to transform div

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,42 @@
 // Document ready jQuery syntax
 $(function() {
-  // Attaching event listeners and prevent default event action on click
-  function attachListeners() {
-    $('#read-reviews').click(function(e) {
-      e.preventDefault()
-    });
-
-    $('#write-reviews').click(function(e) {
-      e.preventDefault()
-    });
-  }
+  // Attaching event listeners on document ready
+  attachListeners()
 });
+
+function attachListeners() {
+  // Prevent event default and send clicked element to the expandWindow function
+  $('#read-reviews').click(function(e) {
+    e.preventDefault();
+    expandWindow(this.parentElement.nextElementSibling);
+    getReviews();
+  });
+
+  // Prevent event default and send clicked element to the expandWindow function
+  $('#write-reviews').click(function(e) {
+    e.preventDefault();
+    expandWindow(this.parentElement.previousElementSibling);
+  });
+}
+// Add classes on click to container div and anchor inside div
+function expandWindow(object) {
+  $(object).find('a').addClass('animate-text');
+  $(object).addClass('animate-div');
+}
+
+function getReviews() {
+  $.get('/reviews', function(response) {
+    response.forEach(function(review) {
+      let newReview = new Review(review['title'], review['description'], review['rating'])
+      debugger;
+    });
+  })
+}
+
+class Review {
+  constructor(title, description, rating) {
+    this.title = title;
+    this.description = description;
+    this.rating = rating;
+  }
+}
