2386076855752fa680ed3b674f8f82c9e8661a7c
Sam Ojling <s.ojling@gmail.com>
2018-04-23 14:27:36 +0200

    Animation to reveal list of reviews

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,20 +8,22 @@ function attachListeners() {
   // Prevent event default and send clicked element to the expandWindow function
   $('#read-reviews').click(function(e) {
     e.preventDefault();
-    expandWindow(this.parentElement.nextElementSibling);
-    getReviews();
+    expandWindow(this, this.parentElement.nextElementSibling);
+    // Invoke getReviews after 3s to wait for the animation to finish
+    setTimeout(getReviews, 3000)
   });
 
   // Prevent event default and send clicked element to the expandWindow function
   $('#write-reviews').click(function(e) {
     e.preventDefault();
-    expandWindow(this.parentElement.previousElementSibling);
+    expandWindow(this, this.parentElement.previousElementSibling);
   });
 }
 // Add classes on click to container div and anchor inside div
-function expandWindow(object) {
-  $(object).find('a').addClass('animate-text');
-  $(object).addClass('animate-div');
+function expandWindow(mainObject, siblingObject) {
+  $(siblingObject).find('a').addClass('animate-text');
+  $(siblingObject).addClass('animate-div');
+  $(mainObject).addClass('d-none') // Invoke after 1s
 }
 
 function getReviews() {
@@ -34,7 +36,7 @@ function getReviews() {
 
 function appendContent(content) {
   let newReview = new Review(content['title'], content['description'], content['rating']);
-  $('.row').append(
+  $('.left').append(
     `<div>
       <h1>${newReview.title}</h1>
       <h2>${newReview.description}</h2>
