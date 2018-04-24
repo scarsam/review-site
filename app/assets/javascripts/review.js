47fcb73d3ef205de8e47ff175e8fb73588be5a97
Sam Ojling <s.ojling@gmail.com>
2018-04-24 13:09:35 +0200

    storing review ids in an array, check if id already exist to prevent it from getting attached to the DOM multiple times

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,32 +1,48 @@
+let reviewIds = [];
+
 class Review {
-  constructor(title, description, rating) {
+  constructor(id, title, description, rating) {
+    this.id = id;
     this.title = title;
     this.description = description;
     this.rating = rating;
   }
+
+  appendReview() {
+    $('.content .list-group').append(
+      `
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">${this.title}</h5>
+          <small>Updated 3 days ago</small>
+        </div>
+        <p class="mb-1">${this.description}</p>
+        <small>${this.rating}</small>
+      </a>
+      `
+    )
+  }
+}
+
+function isNewReview(review) {
+  return !reviewIds.some(existingId => existingId === review.id)
 }
 
 function getReviews() {
   $.get('/reviews', function(response) {
     response.forEach(function(review) {
-      appendReview(review)
+      if (isNewReview(review)) {createReviews(review)}
     });
   })
 }
 
-function appendReview(review) {
-  let newReview = new Review(review['title'], review['description'], review['rating']);
-  $('.content .list-group').append(
-    `
-    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-      <div class="d-flex w-100 justify-content-between">
-        <h5 class="mb-1">${newReview.title}</h5>
-        <small>3 days ago</small>
-      </div>
-      <p class="mb-1">${newReview.description}</p>
-      <small>${newReview.rating}</small>
-    </a>
-    `
-  )
+function createReviews(review) {
+  let newReview = new Review(
+    review['id'],
+    review['title'],
+    review['description'],
+    review['rating']
+  );
+  reviewIds.push(newReview.id);
+  newReview.appendReview();
 }
-
