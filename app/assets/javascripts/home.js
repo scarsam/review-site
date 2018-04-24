a85ba06f4993863883e92f17a611c6b5fc037991
Sam Ojling <s.ojling@gmail.com>
2018-04-24 08:57:30 +0200

    Updated animation to use jQuery animate method

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,23 +8,23 @@ function attachListeners() {
   // Prevent event default and send clicked element to the expandWindow function
   $('#read-reviews').click(function(e) {
     e.preventDefault();
-    expandWindow(this, this.parentElement.nextElementSibling);
-    // Invoke getReviews after 3s to wait for the animation to finish
-    setTimeout(getReviews, 3000)
+    slideWindow(this, this.parentElement.nextElementSibling);
+    getReviews();
   });
 
   // Prevent event default and send clicked element to the expandWindow function
   $('#write-reviews').click(function(e) {
     e.preventDefault();
-    expandWindow(this, this.parentElement.previousElementSibling);
+    slideWindow(this, this.parentElement.previousElementSibling);
   });
 }
 // Add classes on click to container div and anchor inside div
-function expandWindow(mainObject, siblingObject) {
-  $(siblingObject).find('a').addClass('animate-text');
-  $(siblingObject).addClass('animate-div');
-  $(mainObject).addClass('d-none') // Invoke after 1s
+function slideWindow(mainObject, siblingObject) {
+  $('.content').animate({
+    left: '0%'
+  }, 1000)
 }
+
 
 function getReviews() {
   $.get('/reviews', function(response) {
