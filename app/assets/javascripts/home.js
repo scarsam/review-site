8da8fb569fbcf62be443db74e108cfc67c4efe37
Sam Ojling <s.ojling@gmail.com>
2018-04-25 16:32:31 +0200

    Removed some slide animation and created a general close link which will look at its context as determine what to close

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,34 +8,20 @@ function attachListeners() {
   // Prevent event default and send clicked element to the expandWindow function
   $('#read-reviews').click(function(e) {
     e.preventDefault();
-    slideWindow(this, this.parentElement.nextElementSibling);
     getReviews();
   });
 
   // Prevent event default and send clicked element to the expandWindow function
   $('#write-reviews').click(function(e) {
     e.preventDefault();
-    slideWindow(this, this.parentElement.previousElementSibling);
   });
 
   $('.close').click(function(e) {
     e.preventDefault();
-    closeWindow();
+    closeWindow(this);
   })
 }
-// Add classes on click to container div and anchor inside div
-function slideWindow(mainObject, siblingObject) {
-  $(mainObject).addClass('animate-text');
-  $(siblingObject).find('a').addClass('animate-text');
-  $('.content').animate({
-    left: '0%'
-  }, 1500)
-}
-// Animates content div and slides it away off the screen
-function closeWindow() {
-  $('.content').animate({
-    left: '-100%'
-  }, 1500);
-  $('#write-reviews').removeClass('animate-text');
-  $('#read-reviews').removeClass('animate-text');
+
+function closeWindow(window) {
+  $(window).parents('div.content-container').removeClass('show');
 }
