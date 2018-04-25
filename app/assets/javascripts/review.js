1dffafbb3832e52459d170d7c151c1655d1cc478
Sam Ojling <s.ojling@gmail.com>
2018-04-25 16:33:53 +0200

    Cleaned up some of my DOM append functions and broke up the HTML into sepearte functions, created a protoype method to check how old an review is

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -3,64 +3,68 @@ let reviewsStore = [];
 
 // Review class constructor with some prototype methods to append content to DOM
 class Review {
-  constructor(id, title, description, rating) {
+  constructor(id, title, description, rating, created) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.rating = rating;
+    this.created = created;
+  }
+  // Takes todays date and convert it to MS
+  // Takes reviews created date and converts it to MS
+  // Calculates one day in MS
+  // Calculates the difference and divide it in days
+  // to see how many days since last posted
+  daysSinceCreated() {
+    let todayMs = Date.now();
+    let reviewDateMs = new Date(this.created).getTime();
+    let oneDay = 24 * 60 * 60 * 1000;
+    let diffDays = Math.abs(todayMs - reviewDateMs);
+    return Math.round(diffDays / oneDay);
   }
   // Appending reviews and attaching click event to load the show page
   appendReviews() {
-    $('.content .list-group').append(
-      `
-      <a data-review="${this.id}" id="review-${this.id}" class="review-link list-group-item list-group-item-action flex-column align-items-start">
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">${this.title}</h5>
-          <small>Posted 3 days ago</small>
-        </div>
-        <p class="mb-1">${this.description}</p>
-        <small>${this.rating}</small>
-      </a>
-      `
-    ).on('click', `#review-${this.id}`, function(e) {
-      e.preventDefault();
-      let id = $(this).data('review');
-      getReview(id)
+    $('.reviews-container .reviews')
+      // Calls reviewTemplate function and pass in the object
+      // which returns html with object properties
+      // Bind an click event on each anchor tag that invoked getReview function and passes in the id
+      .append(reviewsTemplate(this))
+      .on('click', `#review-${this.id}`, function(e) {
+        e.preventDefault();
+        let id = $(this).data('review');
+        getReview(id)
     })
   }
-  // Appending review to show page
+  // Appending review to show page by calling reviewTemplate and passing in the object
   appendReview() {
-    $('.content').empty().append(
-      `
-        <div class="d-flex w-100">
-          <h5 class="mb-1">${this.title}</h5>
-          <small>Updated 3 days ago</small>
-          <p class="mb-1">${this.description}</p>
-          <small>${this.rating}</small>
-        </div>
-      `
-    )
+    $('.review-container .review').empty().append(reviewTemplate(this))
   }
 }
+
 // Get request to get all reviews from the index action
 function getReviews() {
   $.get('/reviews', function(response) {
     response.forEach(function(review) {
       if (isNewReview(review)) {createReviews(review)}
     });
+    $('.reviews-container').addClass('show');
   })
 }
+
 // Get request to get a single review from the show action
 function getReview(id) {
   $.get('/reviews/' + id, function(response) {
     let findReview = reviewsStore.find(review => review.id === response.id);
-    findReview.appendReview()
+    findReview.appendReview();
+    $('.review-container').addClass('show');
   })
 }
+
 // Check if review is already in the store, if so return false
 function isNewReview(review) {
   return !reviewsStore.some(existingReview => existingReview.id === review.id)
 }
+
 // Function to instantiate a new review based on request response
 // Push that review to the store and append it to the DOM
 function createReviews(review) {
@@ -68,9 +72,35 @@ function createReviews(review) {
     review['id'],
     review['title'],
     review['description'],
-    review['rating']
+    review['rating'],
+    review['created_at']
   );
   reviewsStore.push(newReview);
   newReview.appendReviews();
 }
 
+// HTML template for reviews
+function reviewsTemplate(object) {
+  return $.parseHTML(`
+    <a data-review="${object.id}" id="review-${object.id}" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">${object.title}</h5>
+      <small>Posted ${object.daysSinceCreated()} day(s) ago</small>
+    </div>
+      <p class="mb-1">${object.description}</p>
+      <small>${object.rating}</small>
+    </a>
+  `)
+}
+
+// HTML template for review
+function reviewTemplate(object) {
+  return $.parseHTML(`
+    <div class="d-flex w-100">
+      <h5 class="mb-1">${object.title}</h5>
+      <small>Posted ${object.daysSinceCreated()} day(s) ago</small>
+      <p class="mb-1">${object.description}</p>
+      <small>${object.rating}</small>
+    </div>
+  `)
+}
